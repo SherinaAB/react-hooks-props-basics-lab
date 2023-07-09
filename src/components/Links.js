fa86c27ab92edf540be789f1ac1246d9074bc3c6
Sherina Buenaseda <sherina76@gmail.com>
2023-07-09 14:23:57 -0600

    completed lab

--- src/components/Links.js
+++ src/components/Links.js
@@ -7,7 +7,7 @@ function Links(props) {
                 <a href={props.github}>{props.github}</a>
                 <a href={props.linkedin}>{props.linkedin}</a>
         </div>
-    );
+    )
 }
 
 export default Links;
